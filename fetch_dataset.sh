99d976be9556f9a96b01c49f566475b942e0978c
Bernardo Estrada <49007495+BernardoEstrada@users.noreply.github.com>
2023-09-19 17:48:43 -0600

    partition repo

---
+++ fetch_dataset.sh
@@ -0,0 +1,3 @@
+# TODO: add to README
+# wget -c https://www.physionet.org/static/published-projects/eegmmidb/eeg-motor-movementimagery-dataset-1.0.0.zip
+# wget -r -N -c -np https://physionet.org/files/eegmmidb/1.0.0/
